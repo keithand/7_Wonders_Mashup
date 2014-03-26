9755ef57ea19c8960e640f800ec859e1be1b3a90
Keith Anderson <keithanderson@boldier.com>
2014-03-25 18:59:11 -0700

    Version 1.01
    Added steps for the Twitter API implementation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-//set the map 
+//set the google map api custom settings 
 function initialize () {
 	var mapOptions = {
 		center: new google.maps.LatLng(50.976286, -38.828125),
@@ -19,15 +19,17 @@ function initialize () {
 			}
 		};
 
-
+	//attach the HTML class 'google_map' to the google map api
 	var map = new google.maps.Map(document.getElementById('google_map'),
 		mapOptions);
+
 
 	setMarkers(map, wonders);
 
 
 }
 
+	//list of the markers to show on the map
 	var wonders = [
 		['Chunnel', 49.589, 1.2304, 1],
 		['CN Tower', 43.653, -79.3831, 2],
@@ -38,7 +40,7 @@ function initialize () {
 		['Islands of Dubai', 24.998427, 54.992853, 7]
 	];
 
-
+//this function sets the location and appearance of the wonders object by using a for loop
 function setMarkers(map, locations) {
 	for (var i = 0; i < locations.length; i++){
 		var mark = locations[i];
@@ -57,11 +59,31 @@ function setMarkers(map, locations) {
 
 } //closing for setMarkers function
 
+
+//event listener for the infowindow (if will use)
 google.maps.event.addListener(wonders, 'click', function(){
 	infowindow.open(map, marker);
 });
 
+//event listener for the map, which happens when the document is loaded
 google.maps.event.addDomListener(window, 'load', initialize)
+
+
+
+
+/////////SETTING UP THE TWITTER API//////////
+
+//call the AJAX GET request for the information to be pulled from the Twitter servers. This will include
+// the AJAX request and the resulting information (https://dev.twitter.com/docs/twitter-libraries)
+
+//set .done() and .fail() functions for what to do if the sit connects correctly to the Twitter server
+
+//create variables that correspond to the data that we are picking up from the API
+
+//display data in the .tweet class in the HTML. make sure you .clone the class
+
+//Use $(document).ready(function(()})) to make the site interact with the elements from the API
+
 
 
 
